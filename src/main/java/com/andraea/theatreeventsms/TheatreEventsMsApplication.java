f6c15a0d8bc2f33ce97323fea32f17e699a6b681
Andraea Zambakides <azambakides@gmail.com>
2021-08-15 17:09:16 +1000

    Initial commit with all the files, after creating the repository from selecting "Share Project on GitHub" after setting up a New Project with Spring Initializer with Web, H2, and JPA dependencies and created a README.md from scratch

---
+++ src/main/java/com/andraea/theatreeventsms/TheatreEventsMsApplication.java
@@ -0,0 +1,13 @@
+package com.andraea.theatreeventsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreEventsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheatreEventsMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andraea/theatreeventsms/TheatreEventsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andraea.theatreeventsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatreEventsMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
